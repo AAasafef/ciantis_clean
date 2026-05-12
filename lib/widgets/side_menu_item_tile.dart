a40236d12735a950063e70af01cc84ba38bec801
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-12 12:37:09 -0400

    Update side_menu_item_tile.dart

--- lib/widgets/side_menu_item_tile.dart
+++ lib/widgets/side_menu_item_tile.dart
@@ -2,46 +2,82 @@ import 'package:flutter/material.dart';
 
 import '../theme/ciantis_theme.dart';
 
+import 'notification_badge.dart';
+
 class SideMenuItemTile extends StatelessWidget {
   final IconData icon;
+
   final String title;
-  final VoidCallback? onTap;
+
+  final VoidCallback onTap;
+
+  final String? badgeCount;
 
   const SideMenuItemTile({
     super.key,
     required this.icon,
     required this.title,
-    this.onTap,
+    required this.onTap,
+    this.badgeCount,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.only(
-          bottom: 22,
-        ),
-        child: Row(
-          children: [
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: 10,
+      ),
+      child: GestureDetector(
+        onTap: onTap,
 
-            Icon(
-              icon,
-              color: CiantisTheme.white,
-              size: 24,
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 18,
+            vertical: 16,
+          ),
+
+          decoration: BoxDecoration(
+            color:
+                Colors.white.withOpacity(
+              .06,
             ),
 
-            const SizedBox(width: 16),
+            borderRadius:
+                BorderRadius.circular(
+              18,
+            ),
+          ),
 
-            Text(
-              title,
-              style: const TextStyle(
-                color: CiantisTheme.white,
-                fontSize: 17,
-                fontWeight: FontWeight.w300,
+          child: Row(
+            children: [
+
+              Icon(
+                icon,
+                color:
+                    CiantisTheme.white,
               ),
-            ),
-          ],
+
+              const SizedBox(width: 16),
+
+              Expanded(
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color:
+                        CiantisTheme.white,
+                    fontSize: 16,
+                    fontWeight:
+                        FontWeight.w300,
+                  ),
+                ),
+              ),
+
+              if (badgeCount != null)
+                NotificationBadge(
+                  count: badgeCount!,
+                ),
+            ],
+          ),
         ),
       ),
     );
