ec52f42843ddaaabf88631199dc331a5a39aa6fe
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-12 12:17:58 -0400

    Update luxury_card.dart

--- lib/widgets/luxury_card.dart
+++ lib/widgets/luxury_card.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 import '../theme/ciantis_theme.dart';
+
 import 'glass_container.dart';
+import 'luxury_divider.dart';
 
 class LuxuryCard extends StatelessWidget {
   final IconData icon;
+
   final String title;
+
   final String subtitle;
+
   final Widget? trailing;
+
   final VoidCallback? onTap;
 
   const LuxuryCard({
@@ -27,42 +33,75 @@ class LuxuryCard extends StatelessWidget {
       ),
       child: GestureDetector(
         onTap: onTap,
+
         child: GlassContainer(
-          padding: const EdgeInsets.all(18),
-          borderRadius: CiantisTheme.radiusLarge,
-          child: Row(
+          padding: const EdgeInsets.all(
+            18,
+          ),
+
+          borderRadius:
+              CiantisTheme.radiusLarge,
+
+          child: Column(
             children: [
-              Icon(
-                icon,
-                color: CiantisTheme.white,
-                size: 30,
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      title,
-                      style: const TextStyle(
-                        color: CiantisTheme.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w300,
-                      ),
+
+              Row(
+                children: [
+
+                  Icon(
+                    icon,
+                    color:
+                        CiantisTheme.white,
+                    size: 30,
+                  ),
+
+                  const SizedBox(width: 16),
+
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment
+                              .start,
+                      children: [
+
+                        Text(
+                          title,
+                          style:
+                              const TextStyle(
+                            color:
+                                CiantisTheme
+                                    .white,
+                            fontSize: 18,
+                            fontWeight:
+                                FontWeight
+                                    .w300,
+                          ),
+                        ),
+
+                        const SizedBox(
+                          height: 5,
+                        ),
+
+                        Text(
+                          subtitle,
+                          style:
+                              const TextStyle(
+                            color:
+                                CiantisTheme
+                                    .whiteSoft,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 5),
-                    Text(
-                      subtitle,
-                      style: const TextStyle(
-                        color: CiantisTheme.whiteSoft,
-                        fontSize: 13,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+
+                  if (trailing != null)
+                    trailing!,
+                ],
               ),
-              if (trailing != null) trailing!,
+
+              const LuxuryDivider(),
             ],
           ),
         ),
