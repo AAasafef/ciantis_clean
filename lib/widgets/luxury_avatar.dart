b913ee28e376d791b9079d4ab66737c946f8dea6
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-12 13:06:18 -0400

    Add imagePath support to LuxuryAvatar widget
    Added optional imagePath to LuxuryAvatar for customizable avatar images.

--- lib/widgets/luxury_avatar.dart
+++ lib/widgets/luxury_avatar.dart
@@ -5,23 +5,38 @@ import '../theme/ciantis_theme.dart';
 class LuxuryAvatar extends StatelessWidget {
   final double radius;
 
+  final String? imagePath;
+
   const LuxuryAvatar({
     super.key,
     this.radius = 34,
+    this.imagePath,
   });
 
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
       radius: radius,
+
       backgroundColor:
           CiantisTheme.profileBrown,
 
-      child: Icon(
-        Icons.person,
-        color: CiantisTheme.white,
-        size: radius,
-      ),
+      backgroundImage:
+          imagePath != null &&
+                  imagePath!.isNotEmpty
+              ? AssetImage(imagePath!)
+              : null,
+
+      child:
+          imagePath == null ||
+                  imagePath!.isEmpty
+              ? Icon(
+                  Icons.person,
+                  color:
+                      CiantisTheme.white,
+                  size: radius,
+                )
+              : null,
     );
   }
 }
