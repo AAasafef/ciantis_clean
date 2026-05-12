71a3e61d60743cf8e2f332e258486139bc3a4a78
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-12 10:41:21 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'theme/ciantis_theme.dart';
 
-import 'widgets/bottom_nav_bar.dart';
-import 'widgets/grid_menu.dart';
-import 'widgets/home_panel.dart';
-import 'widgets/side_menu.dart';
-import 'widgets/top_bar.dart';
+import 'screens/dashboard/dashboard_screen.dart';
 
 void main() {
   runApp(const CiantisApp());
@@ -19,120 +15,12 @@ class CiantisApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       title: 'Ciantis',
+
       theme: CiantisTheme.themeData,
+
       home: const DashboardScreen(),
-    );
-  }
-}
-
-class DashboardScreen extends StatefulWidget {
-  const DashboardScreen({super.key});
-
-  @override
-  State<DashboardScreen> createState() =>
-      _DashboardScreenState();
-}
-
-class _DashboardScreenState
-    extends State<DashboardScreen> {
-
-  final GlobalKey<ScaffoldState>
-      scaffoldKey =
-      GlobalKey<ScaffoldState>();
-
-  bool showGridMenu = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-
-      // SIDE MENU
-      drawer: const SideMenu(),
-
-      body: Stack(
-        children: [
-
-          // BACKGROUND
-          _background(),
-
-          // OVERLAY
-          _overlay(),
-
-          SafeArea(
-            child: Column(
-              children: [
-
-                // TOP BAR
-                TopBar(
-                  showGridMenu:
-                      showGridMenu,
-
-                  onMenuPressed: () {
-                    scaffoldKey
-                        .currentState
-                        ?.openDrawer();
-                  },
-
-                  onGridPressed: () {
-                    setState(() {
-                      showGridMenu =
-                          !showGridMenu;
-                    });
-                  },
-                ),
-
-                const Spacer(),
-
-                // HOME PANEL
-                if (!showGridMenu)
-                  const HomePanel(),
-
-                // GRID MENU
-                if (showGridMenu)
-                  const GridMenu(),
-
-                const SizedBox(
-                  height: 110,
-                ),
-              ],
-            ),
-          ),
-
-          // BOTTOM NAV BAR
-          BottomNavBar(
-            showGridMenu:
-                showGridMenu,
-
-            onMenuPressed: () {
-              setState(() {
-                showGridMenu =
-                    !showGridMenu;
-              });
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  // BACKGROUND
-  Widget _background() {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient:
-            CiantisTheme.mainGradient,
-      ),
-    );
-  }
-
-  // OVERLAY
-  Widget _overlay() {
-    return Container(
-      color: Colors.black.withOpacity(
-        CiantisTheme.overlayOpacity,
-      ),
     );
   }
 }
