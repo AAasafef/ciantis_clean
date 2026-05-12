cd96d5c917ced418fadcb238afbfbc241d74a729
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-12 13:22:14 -0400

    Create category_picker_chip.dart

---
+++ lib/widgets/category_picker_chip.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../theme/ciantis_theme.dart';
+
+class CategoryPickerChip extends StatelessWidget {
+  final String text;
+  final bool selected;
+  final VoidCallback onTap;
+
+  const CategoryPickerChip({
+    super.key,
+    required this.text,
+    required this.selected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12,
+        ),
+        decoration: BoxDecoration(
+          color: selected
+              ? Colors.white.withOpacity(.22)
+              : Colors.white.withOpacity(.08),
+          borderRadius: BorderRadius.circular(18),
+          border: Border.all(
+            color: selected
+                ? CiantisTheme.white
+                : Colors.white.withOpacity(.08),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: CiantisTheme.white,
+            fontSize: 13,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
+    );
+  }
+}
