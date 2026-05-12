810360bdebb900aba4b9b7582ba39aae7179eef5
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-12 10:44:38 -0400

    Add optional screen property to AppItem class

--- lib/data/app_items.dart
+++ lib/data/app_items.dart
@@ -2,73 +2,14 @@ import 'package:flutter/material.dart';
 
 class AppItem {
   final String title;
+
   final IconData icon;
+
+  final Widget? screen;
 
   const AppItem({
     required this.title,
     required this.icon,
+    this.screen,
   });
 }
-
-const List<AppItem> ciantisApps = [
-
-  AppItem(
-    title: 'Home',
-    icon: Icons.home_outlined,
-  ),
-
-  AppItem(
-    title: 'Goals',
-    icon: Icons.favorite_border,
-  ),
-
-  AppItem(
-    title: 'Habits',
-    icon: Icons.check_circle_outline,
-  ),
-
-  AppItem(
-    title: 'Journal',
-    icon: Icons.menu_book_outlined,
-  ),
-
-  AppItem(
-    title: 'Self Care',
-    icon: Icons.spa_outlined,
-  ),
-
-  AppItem(
-    title: 'Finances',
-    icon: Icons.account_balance_wallet_outlined,
-  ),
-
-  AppItem(
-    title: 'Education',
-    icon: Icons.school_outlined,
-  ),
-
-  AppItem(
-    title: 'Wellness',
-    icon: Icons.monitor_heart_outlined,
-  ),
-
-  AppItem(
-    title: 'Projects',
-    icon: Icons.folder_outlined,
-  ),
-
-  AppItem(
-    title: 'Calendar',
-    icon: Icons.calendar_month_outlined,
-  ),
-
-  AppItem(
-    title: 'Notes',
-    icon: Icons.edit_note_outlined,
-  ),
-
-  AppItem(
-    title: 'Resources',
-    icon: Icons.widgets_outlined,
-  ),
-];
